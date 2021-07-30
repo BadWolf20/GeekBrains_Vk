13db023fbdf57d564113fa8697805b37ecaca405
Roman <Roman.Pichugin20@gmail.com>
2021-07-30 23:53:57 +0700

    Initial Commit

---
+++ vk/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  vk
+//
+//  Created by Roman on 30.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
